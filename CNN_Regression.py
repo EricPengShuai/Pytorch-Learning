b0f22bc446f1a3b7db3225554b77a31a47b8727a
彭帅 <pengshuai@pengshuaideMacBook-Air.local>
2021-03-12 14:51:17 +0800

    nn.CNN for sin(x) --> cos(x)

--- CNN_Regression.py
+++ CNN_Regression.py
@@ -1,38 +1,16 @@
 """
-    作者：Troublemaker
-    日期：2020/4/11 10:59
-    脚本：rnn_regression.py
+    来源：莫凡Pytorch教学
+    源作者：Troublemaker
+        日期：2020/4/11 10:59
+        脚本：rnn_regression.py
+    修改者：EricPengShuai
+    模拟sin函数逼近cos函数
 """
 import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-class LSTM(nn.Module):
-    """搭建rnn网络"""
-    def __init__(self):
-        super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(
-            input_size=input_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            batch_first=True,)
-        self.output_layer = nn.Linear(in_features=hidden_size, out_features=output_size)
-
-    def forward(self, x, hc):
-        # x (batch, time_step, input_size)
-        # h_state (n_layers, batch, hidden_size)
-        # rnn_out (batch, time_step, hidden_size)
-        rnn_out, hc = self.lstm(x, hc)   # h_state是之前的隐层状态
-
-        out = []
-        for time in range(rnn_out.size(1)):
-            every_time_out = rnn_out[:, time, :]       # 相当于获取每个时间点上的输出，然后过输出层
-            temp = self.output_layer(every_time_out)
-            out.append(temp)
-            # print(f'Time={time}', rnn_out.shape, every_time_out.shape, temp.shape, len(out))
-        return torch.stack(out, dim=1), hc       # torch.stack扩成[1, output_size, 1]
+import time
 
 class RNN(nn.Module):
     """搭建rnn网络"""
@@ -78,16 +56,13 @@ y_np = np.cos(steps)
 # plt.legend(loc='best')
 # plt.show()
 
-# rnn = RNN()
-# print(rnn)
-
-lstm = LSTM()
-print(lstm)
+rnn = RNN()
+print(rnn)
 
 # 设置优化器和损失函数
-# optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
-optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
 loss_function = nn.MSELoss()
+startTime = time.time()
 
 plt.figure(1, figsize=(12, 5))
 plt.ion()
@@ -105,15 +80,13 @@ for step in range(train_step):
     y = torch.from_numpy(y_np[np.newaxis, :, np.newaxis])
     # print('x y shape:', x.shape, y.shape)
     
-    # pridect, h_state = rnn(x, h_state)
-    # h_state = h_state.detach()     # 重要！！！ 需要将该时刻隐藏层的状态作为下一时刻rnn的输入
+    pridect, h_state = rnn(x, h_state)
+    h_state = h_state.detach()     # 重要！！！ 需要将该时刻隐藏层的状态作为下一时刻rnn的输入
 
-    pridect, h_state = lstm(x, h_state)
-    h_state = (h_state[0].detach(), h_state[1].detach())
-    # print('shape:', pridect.shape, y.shape)
-    # exit()
-    # if step == 50:
-    #     exit()
+    if step == train_step - 1:
+        endTime = time.time()
+        print(f'TimeSum={round(endTime - startTime, 4)}s')
+        # exit()
     loss = loss_function(pridect, y)
     optimizer.zero_grad()
 
@@ -128,5 +101,4 @@ for step in range(train_step):
 
 plt.ioff()
 plt.show()
-
 
